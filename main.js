d22932ad1deb7cf012b0334da3fa135591500c6e
hamid Hazzab <hamid.hazzab@lapiscine.pro>
2023-11-06 12:49:29 +0100

    add toggle on the buttons

--- main.js
+++ main.js
@@ -1,15 +1,35 @@
 
+// je fait appel aux éléments présent dans la div card
 const cards = document.querySelectorAll(`.card`);
 
+// je veux parcourir chaque élément de la variable Cards
 cards.forEach((dogCards)=>{
 
-   dogCards.addEventListener(`click`,()=>{
+    // je cherche a définir une action au click
 
-       const availability = dogCards.querySelector(`.availability`);
+   dogCards.addEventListener(`click`,()=>{
+     //  je sélectionne  la classe ".availability" que je passe en variable
+
+     const availability = dogCards.querySelector(`.availability`);
+
+
+// je cherche a définir les conditions pour l'affichage ou non du texte (availability)
+    if (availability.style.display == "none"){
+
+       // je change ses propriétés css afin que le texte apparaisse a l'écran
        
        availability.style.display = "block";
+    } else {
+
+        // je change ses propriétés css afin que le texte n'apparaisse pas a l'écran
+
+        availability.style.display = "none";
+    }
+       
     })
 })
+
+
 
 
 
